490e4004cadcf341e607fc6757d538e581c7c5d7
Oje Dominion <dominionoje@gmail.com>
2022-09-26 18:18:22 -0700

    Print the maximum number in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     High_score = 0
+    for d in range(0, len(my_list)):
+        my_list[d]=int(my_list[d])
     for score in my_list:
         if len(my_list) == 0:
             return None
