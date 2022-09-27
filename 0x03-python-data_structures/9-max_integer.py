e8ddc6d9b87995d17ffa5692d61ef8b93b3ca763
Oje Dominion <dominionoje@gmail.com>
2022-09-26 18:21:40 -0700

    Adjusted

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    High_score = 0
-    for d in range(0, len(my_list)):
-        my_list[d]=int(my_list[d])
-    for score in my_list:
-        if len(my_list) == 0:
-            return None
-        if (score > High_score):
-            High_score = score
-    return High_score
-
+    if len(my_list) == 0:
+        return (None)
+    else:
+        max_int = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max_int:
+                max_int = my_list[i]
+        return (max_int)
