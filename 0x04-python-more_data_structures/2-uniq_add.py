38c9011b2399a8fade3781d4d1aebeda753c89cc
Oje Dominion <dominionoje@gmail.com>
2022-09-28 16:04:22 -0700

    A little indentation error fixed

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new_nos  = 0
-    unique_numbers = set(my_list)
-    for x in unique_numbers:
+    for x in set(my_list):
         new_nos += x
     return new_nos
