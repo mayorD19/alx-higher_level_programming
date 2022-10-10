d230d279f8d9776085807afd2ca276ced34e1193
Oje Dominion <dominionoje@gmail.com>
2022-10-10 16:24:54 -0700

    Print a list safely

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""Making a function that prints the number of items in a/
-list and raising or making an exception to it.
-"""
 def safe_print_list(my_list=[], x=0):
     a = 0
     try:
