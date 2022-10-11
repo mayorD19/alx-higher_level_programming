a9e2e53617d3f1c4be45c9674787f268645f1b70
Oje Dominion <dominionoje@gmail.com>
2022-10-11 16:22:02 -0700

    Use the getter and setter properties in an OOP

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,5 +4,8 @@ class Square:
     """This represents the class created"""
     def __init__(self, size):
         """This is the Initialized method which has a 
-        field/attribute called size"""
+        field/attribute called size.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
