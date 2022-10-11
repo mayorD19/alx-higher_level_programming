8feb3e16b837ddbb0ba51a1f4550fa434ce62870
Oje Dominion <dominionoje@gmail.com>
2022-10-11 15:59:24 -0700

    Initialized a new square and gave it a new field or attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Creating a class named Square"""
+class Square:
+    """This represents the class created"""
+    def __init__(self, size):
+        """This is the Initialized method which has a 
+        field/attribute called size"""
+        self.__size = size
