b2789f5e5cb83849ed357cf8b3878d0d379eed84
Oje Dominion <dominionoje@gmail.com>
2022-10-25 18:58:47 -0700

    Adding Solutions from Task 3-5

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A function that imports from the json library and
+returns the JSON representation of an object(string)
+"""
+
+
+import json
+
+
+
+def to_json_string(my_obj):
+    """The Module that returns the JSON representation of the object
+    """
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A function that returns the str from a JSON representation
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """A return of the string(A python object) 
+    from a JSON representation
+    """
+    return json.loads(my_str)
