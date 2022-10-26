7705632d9ad91de158457d4564c620df70e2122c
Oje Dominion <dominionoje@gmail.com>
2022-10-25 19:04:59 -0700

    Little changes made to appease pycodestyle

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,8 +6,10 @@ from a JSON representation
 import json
 
 
+
 def from_json_string(my_str):
-    """A return of the string(A python object) 
+    """
+    A return of the string(A python object) 
     from a JSON representation
     """
     return json.loads(my_str)
