f121920434447b8d721615e94ef04143d228c572
Oje Dominion <dominionoje@gmail.com>
2022-10-25 17:59:07 -0700

    Encoding style added

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""A function that reads a text"""
+"""A function that reads a text
+"""
 
 
 def read_file(filename="my_file_0.txt"):
     """A function that reads a text file using the
-    with keyword for easy clean-up"""
+    with keyword for easy clean-up
+    """
 
-    with open(filename) as f:
+    with open(filename, "r", encoding = "utf-8") as f:
         for line in f:
-            print(line, end="")
+            print(f.read() end="")
 
